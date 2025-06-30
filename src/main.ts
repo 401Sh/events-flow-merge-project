f8b337db08ee1a094264cdd80b8adf81a5bb66c0
401Sh <barash1a@gmail.com>
2025-06-30 14:22:53 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { Logger, LogLevel } from '@nestjs/common';
+
+dotenv.config();
+
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+const logLevels = process.env.LOG_LEVEL?.split(',') as LogLevel[] || ['log', 'error', 'warn'];
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useLogger(logLevels);
+  
+  await app.listen(port, host).then(() => {
+    Logger.log(`http://${host}:${port} - server start`);
+  });
+}
+bootstrap();

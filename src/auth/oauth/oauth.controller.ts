d69189e7ffee04b042f86828eebd9e350d03fcc2
401Sh <barash1a@gmail.com>
2025-07-12 13:31:37 +0000

    feat: create GET redirect route

--- src/auth/oauth/oauth.controller.ts
+++ src/auth/oauth/oauth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseEnumPipe, Res } from '@nestjs/common';
 import { OAuthService } from './oauth.service';
+import { Response } from 'express';
+import { EventAPISource } from 'src/events/enums/event-source.enum';
 
 @Controller('oauth')
 export class OAuthController {
   constructor(private readonly oAuthService: OAuthService) {}
+
+  @Get('redirect/:source')
+  oauthRedirect(
+    @Res() res: Response,
+    @Param('source', new ParseEnumPipe(EventAPISource)) source: EventAPISource,
+  ) {
+    const url = this.oAuthService.getRedircetUrl(source);
+    return res.redirect(url);
+  }
 }
--- src/auth/oauth/oauth.service.ts
+++ src/auth/oauth/oauth.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EventAPISource } from 'src/events/enums/event-source.enum';
 
 @Injectable()
-export class OAuthService {}
+export class OAuthService {
+  private readonly logger = new Logger(OAuthService.name);
+
+  constructor(private readonly configService: ConfigService) {}
+
+  getRedircetUrl(source: EventAPISource) {
+    // TODO: вынести редирект в конфиг или env и заменить на реальное значение
+    const redirectUrl = `http://localhost:3000/oauth/callback/${source}`;
+    const encodedRedirectUrl = encodeURIComponent(redirectUrl);
+
+    const urlParts = this.getSourceRedirectUrlParts(source);
+
+    const authUrl = 
+      `${urlParts.baseURL}authorize?client_id=${urlParts.clientId}` + 
+      `&redirect_uri=${encodedRedirectUrl}&response_type=${urlParts.type}`;
+
+    this.logger.debug(`${source} authorize redirect url: `, authUrl);
+    return authUrl;
+  }
+
+
+  getSourceRedirectUrlParts(source: EventAPISource) {
+    if (source == EventAPISource.LEADER_ID) {
+      // TODO: привести все leader url к одному виду? (проблема в /apps/v1 и /apps)
+      const baseURL = 'https://leader-id.ru/apps/';
+      const clientId = this.configService.getOrThrow<string>('LEADER_CLIENT_ID');
+      
+      return {
+        baseURL: baseURL,
+        clientId: clientId,
+        type: 'code',
+      };
+    }
+    
+    // TODO: привести все timepad url к одному виду? (проблема в /v1 и /oauth)
+    // oauth timepad в закрытом тестировании (12.07.2025)
+    const baseURL = 'https://api.timepad.ru/oauth/';
+    const clientId = this.configService.getOrThrow<string>('LEADER_CLIENT_ID');
+    
+    return {
+      baseURL: baseURL,
+      clientId: clientId,
+      type: 'token',
+    };
+  }
+}

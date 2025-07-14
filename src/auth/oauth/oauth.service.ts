98170755f594b28827b805344445989661c08b4c
401Sh <barash1a@gmail.com>
2025-07-14 12:59:34 +0000

    refactor: remove url from oauth.service to env

--- src/auth/oauth/oauth.service.ts
+++ src/auth/oauth/oauth.service.ts
@@ -8,20 +8,27 @@ import { OAuthLeaderHelper } from './oauth-leader-helper';
 export class OAuthService {
   private readonly logger = new Logger(OAuthService.name);
 
+  private readonly appUrl: string;
+  private readonly leaderOAuthUrl: string;
+  private readonly timepadOAuthUrl: string;
+
   constructor(
     private readonly configService: ConfigService,
     private readonly oAuthLeaderHelper: OAuthLeaderHelper,
-  ) {}
+  ) {
+    this.appUrl = this.configService.getOrThrow("APP_BASE_URL");
+    this.leaderOAuthUrl = this.configService.getOrThrow("LEADER_OAUTH_URL");
+    this.timepadOAuthUrl = this.configService.getOrThrow("TIMEPAD_OAUTH_URL");
+  }
 
   getRedircetUrl(source: EventAPISource) {
-    // TODO: вынести редирект в конфиг или env и заменить на реальное значение
-    const redirectUrl = `http://localhost:3000/oauth/callback/${source}`;
+    const redirectUrl = `${this.appUrl}/api/v1/oauth/callback/${source}`;
     const encodedRedirectUrl = encodeURIComponent(redirectUrl);
 
     const urlParts = this.getSourceRedirectUrlParts(source);
 
     const authUrl = 
-      `${urlParts.baseURL}authorize?client_id=${urlParts.clientId}` + 
+      `${urlParts.baseURL}/authorize?client_id=${urlParts.clientId}` + 
       `&redirect_uri=${encodedRedirectUrl}&response_type=${urlParts.type}`;
 
     this.logger.debug(`${source} authorize redirect url: `, authUrl);
@@ -49,24 +56,19 @@ export class OAuthService {
 
   getSourceRedirectUrlParts(source: EventAPISource) {
     if (source == EventAPISource.LEADER_ID) {
-      // TODO: привести все leader url к одному виду? (проблема в /apps/v1 и /apps)
-      const baseURL = 'https://leader-id.ru/apps/';
       const clientId = this.configService.getOrThrow<string>('LEADER_CLIENT_ID');
       
       return {
-        baseURL: baseURL,
+        baseURL: this.leaderOAuthUrl,
         clientId: clientId,
         type: 'code',
       };
     }
-    
-    // TODO: привести все timepad url к одному виду? (проблема в /v1 и /oauth)
-    // oauth timepad в закрытом тестировании (12.07.2025)
-    const baseURL = 'https://api.timepad.ru/oauth/';
+
     const clientId = this.configService.getOrThrow<string>('LEADER_CLIENT_ID');
     
     return {
-      baseURL: baseURL,
+      baseURL: this.timepadOAuthUrl,
       clientId: clientId,
       type: 'token',
     };

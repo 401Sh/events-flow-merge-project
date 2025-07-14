86410e16d0faeb79c77d52af06b0f8c992e5f35d
401Sh <barash1a@gmail.com>
2025-07-14 16:31:41 +0000

    feat: add swagger to oauth routes

---
+++ src/auth/dto/token-response.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+export class TokenResponseDto {
+  @ApiProperty({ type: String })
+  @IsString()
+  accessToken: string;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  source: string;
+}
--- src/auth/oauth/oauth.controller.ts
+++ src/auth/oauth/oauth.controller.ts
@@ -4,11 +4,26 @@ import { Response } from 'express';
 import { EventAPISource } from 'src/events/enums/event-source.enum';
 import { CallbackDto } from '../dto/callback.dto';
 import { refreshCookieOptions } from 'src/configs/cookie.config';
+import { ApiCookieAuth, ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { TokenResponseDto } from '../dto/token-response.dto';
 
 @Controller('oauth')
 export class OAuthController {
   constructor(private readonly oAuthService: OAuthService) {}
 
+  @ApiOperation({
+    summary: 'Получить редирект url на leaderId или timepad',
+  })
+  @ApiParam({
+    name: 'source',
+    required: true,
+    description: 'Источник мероприятий: leaderId или timepad',
+    example: 'leaderId',
+  })
+  @ApiResponse({
+    status: 302,
+    description: 'Редирект на страницу авторизации leaderId или timepad',
+  })
   @Get('redirect/:source')
   oauthRedirect(
     @Res() res: Response,
@@ -19,6 +34,20 @@ export class OAuthController {
   }
 
 
+  @ApiOperation({
+    summary: 'callback для обмена code от leaderId на access и refresh токены',
+  })
+  @ApiQuery({
+    name: 'code',
+    required: true,
+    description: 'Код для обмена на токены leaderId',
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Access токен для доступа к ресурсам на leaderId',
+    type: TokenResponseDto,
+  })
   @Get('callback/leaderId')
   async oauthLeaderCallback(
     @Query() query: CallbackDto,
@@ -28,10 +57,23 @@ export class OAuthController {
     
     res.cookie('refreshToken', result.refresh_token, refreshCookieOptions);
 
-    return res.json({ accessToken: result.access_token });
+    return res.json({ 
+      accessToken: result.access_token,
+      source: 'leaderId',
+    });
   }
 
 
+  @ApiOperation({
+    summary: 'Обновить токены доступа к leaderId. ' +
+    'Требуется наличие refresh токена в защищенных куки',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Новый access токен для доступа к ресурсам на leaderId',
+    type: TokenResponseDto,
+  })
+  @ApiCookieAuth('refreshToken')
   @Post('refresh/leaderId')
   async oauthLeaderRefresh(
     @Request() req,
@@ -43,6 +85,9 @@ export class OAuthController {
 
     res.cookie('refreshToken', result.refresh_token, refreshCookieOptions);
 
-    return res.json({ accessToken: result.access_token });
+    return res.json({ 
+      accessToken: result.access_token, 
+      source: 'leaderId',
+    });
   }
 }

144e1bcb9719e9d1d1ee61b0406b01e797b36b22
401Sh <barash1a@gmail.com>
2025-07-31 09:52:41 +0300

    refactor: add user api rate limiter

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,15 @@ import { UsersService } from './users.service';
 import { LeaderUserService } from './services/leader-user.service';
 import { ClientAuthModule } from 'src/auth/client-auth/client-auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { LeaderApiRateLimiterService } from 'src/api-utils/leader-api-rate-limiter.service';
 
 @Module({
   imports: [ClientAuthModule, HttpModule],
   controllers: [UsersController],
-  providers: [UsersService, LeaderUserService],
+  providers: [
+    UsersService,
+    LeaderUserService,
+    LeaderApiRateLimiterService,
+  ],
 })
 export class UsersModule {}

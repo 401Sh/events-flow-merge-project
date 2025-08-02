716bf5dbc4baa69d629412696360a55fa49afb73
401Sh <barash1a@gmail.com>
2025-08-02 15:10:51 +0000

    feat: create user and session entities

---
+++ src/auth/entities/refresh-session.entity.ts
@@ -0,0 +1,40 @@
+import { UserEntity } from 'src/users/entities/user.entity';
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  BaseEntity,
+  ManyToOne
+} from 'typeorm';
+
+@Entity('refresh-sessions')
+export class RefreshSessionEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 512, unique: true, nullable: false })
+  refreshToken!: string;
+
+  @Column({ type: 'varchar', length: 512 })
+  userAgent: string;
+
+  @Column({ type: 'varchar', length: 45 })
+  ip: string;
+
+  @Column({ type: 'varchar', length: 512 })
+  fingerprint: string;
+
+  @Column({ type: 'timestamp'})
+  expiresAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(
+    () => UserEntity,
+    (user) => user.refreshSessions,
+    { onDelete: 'CASCADE', nullable: false }
+  )
+  user: UserEntity
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import { RefreshSessionEntity } from 'src/auth/entities/refresh-session.entity';
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn,
+  BaseEntity,
+  OneToMany
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password!: string;
+  
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: true })
+  login?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(
+    () => RefreshSessionEntity,
+    (refreshSession) => refreshSession.user,
+    { cascade: true, onDelete: 'CASCADE' }
+  )
+  refreshSessions: RefreshSessionEntity[];
+}
